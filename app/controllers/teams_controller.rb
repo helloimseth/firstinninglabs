b36e92ff912966c91e99d1c67f09ff5e2f04d175
Seth Hamlin <helloimseth@gmail.com>
2015-05-26 20:31:57 -0400

    implement rudimentary autoupdate

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,6 @@
 class TeamsController < ApplicationController
+  before_action :check_stats
+  
   def index
     @teams = Team.all
   end
@@ -8,4 +10,13 @@ class TeamsController < ApplicationController
                 .includes(pits: :statlines)
                 .find(params[:id])
   end
+
+  private
+    def check_stats
+      if PitStatline.last.created_at < Date.yesterday ||
+         BatStatline.last.created_at < Date.yesterday
+         Pit.refresh_stats!
+         Bat.refresh_stats!
+       end
+    end
 end
